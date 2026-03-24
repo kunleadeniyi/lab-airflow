ef1035825dbb153ff3a36f4802f7737e1aec8905
Ayokunle Adeniyi <Ayokunle.Adeniyi@warwick.ac.uk>
2026-03-24 12:16:24 +0000

    migrating to dbt taskgroup using astronomer-cosmos, updated airflow image too to prevent package conflicts

--- dags/dbt_f1_dag.py
+++ dags/dbt_f1_dag.py
@@ -1,30 +1,45 @@
-from airflow.decorators import dag
-from airflow.operators.bash import BashOperator
-import pendulum
+from pathlib import Path
 
-DBT_DIR = "/opt/airflow/dbt"
-DBT_PROFILES_DIR = "/opt/airflow/dbt/profiles"
+import pendulum
+from airflow.decorators import dag
+from cosmos import DbtTaskGroup, ExecutionConfig, ProfileConfig, ProjectConfig, RenderConfig
+from cosmos.constants import ExecutionMode, LoadMode
+
+DBT_DIR = Path("/opt/airflow/dbt")
+
+project_config = ProjectConfig(
+    dbt_project_path=DBT_DIR,
+)
+
+profile_config = ProfileConfig(
+    profile_name="f1_warehouse",
+    target_name="dev",
+    profiles_yml_filepath=DBT_DIR / "profiles" / "profiles.yml",
+)
+
+execution_config = ExecutionConfig(
+    execution_mode=ExecutionMode.LOCAL,
+    dbt_executable_path=Path("/home/airflow/.local/bin/dbt"),
+)
+
 
 @dag(
     schedule=None,
     start_date=pendulum.datetime(2025, 1, 1),
     catchup=False,
-    tags=['formula1', 'dbt'],
+    tags=["formula1", "dbt"],
     max_active_runs=1,
 )
 def dbt_f1():
-    dbt_silver = BashOperator(
-        task_id="dbt_silver",
-        bash_command=f"cd {DBT_DIR} && dbt run --profiles-dir {DBT_PROFILES_DIR} --select silver.*",
+    DbtTaskGroup(
+        group_id="f1_models",
+        project_config=project_config,
+        profile_config=profile_config,
+        execution_config=execution_config,
+        render_config=RenderConfig(
+            load_method=LoadMode.DBT_LS,  # parses project live via `dbt ls`
+        ),
     )
-    dbt_gold = BashOperator(
-        task_id="dbt_gold",
-        bash_command=f"cd {DBT_DIR} && dbt run --profiles-dir {DBT_PROFILES_DIR} --select gold.*",
-    )
-    dbt_test = BashOperator(
-        task_id="dbt_test",
-        bash_command=f"cd {DBT_DIR} && dbt test --profiles-dir {DBT_PROFILES_DIR}",
-    )
-    dbt_silver >> dbt_gold >> dbt_test
+
 
 dbt_f1()
